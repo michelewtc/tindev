df8031d3b7c60984a043ed44bbd73d98239ee768
michelewtc <michelechan1996@gmail.com>
2020-11-12 15:02:37 -0300

    login page

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import logo from '../assets/logo.svg';
+
+
+export default function Main({ match }) {
+    return (
+        <div className="main-container">
+            <img src={logo} alt="TinDev" />
+            <ul>
+                
+            </ul>
+        </div>
+    ) 
+}
