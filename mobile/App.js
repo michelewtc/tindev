c9982b68aaef9518d35698184b06b1c962ab2055
MiWChan <michelechan1996@gmail.com>
2020-11-24 19:23:21 -0300

    mobile hello world

---
+++ mobile/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  text: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 20
+  },
+});
